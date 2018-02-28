df7852bef4b899dd0990ee21dd21d5bf9dc640e4
Dmitry <lvbnhbq2103@mail.ru>
2018-03-01 01:45:47 +0700

    home

--- src/js/main.js
+++ src/js/main.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
     $(this).toggleClass("header__phones_all--open");
     $(".header__phones_popup").toggleClass("header__phones_popup--open");
   });
+
+  $(".header__cart_container").click(function(){
+    $(".header__cart_popup").toggleClass("header__cart_popup--open");
+  });
 });
