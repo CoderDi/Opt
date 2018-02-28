6041dd94416ba7c2ef58bb18781b59de2c653ee5
Dmitry <lvbnhbq2103@mail.ru>
2018-02-28 16:44:08 +0700

    index

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,6 @@
 $(document).ready(function() {
-  
+  $(".header__phones_all").click(function(){
+    $(this).toggleClass("header__phones_all--open");
+    $(".header__phones_popup").toggleClass("header__phones_popup--open");
+  });
 });
