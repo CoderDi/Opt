5095ca95b28091f9126aaa36bdbc398560158cfa
Dmitry <lvbnhbq2103@mail.ru>
2018-03-02 01:29:05 +0700

    home

--- src/js/main.js
+++ src/js/main.js
@@ -10,4 +10,21 @@ $(document).ready(function() {
   $(".header__cab_nologin").click(function(){
     $(".header__cab_popup").toggleClass("header__cab_popup--open");
   });
+  $(".header__vin_span").click(function(){
+    $(".header__vin_popup").toggleClass("header__vin_popup--open");
+  });
+  $(".minus").click(function(){
+    if (parseInt($(this).parents(".cart__item").find(".value").html()) > 1) {
+      $(this).parents(".cart__item").find(".value").html(parseInt($(this).parents(".cart__item").find(".value").html()) - 1);
+    }
+  });
+  $(".plus").click(function(){
+    $(this).parents(".cart__item").find(".value").html(parseInt($(this).parents(".cart__item").find(".value").html()) + 1);
+  });
+
+  $(".cart__item_delete").click(function(){
+    $(this).parents(".cart__item").find(".cart__item_rem-mes").addClass("cart__item_rem-mes--show");
+    $(this).parents(".cart__item").find(".cart__item_container").remove();
+    
+  });
 });
